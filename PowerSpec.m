5b9d80e398b32e7d4b8b560e4dc3bdb93081de62
Chad <chadblaine512@gmail.com>
2016-10-05 17:56:26 -0500

    Update PowerSpec.m

--- PowerSpec.m
+++ PowerSpec.m
@@ -2,8 +2,7 @@ function PowerSpec(shotn)
 
 	%Define constants
     skipLines=1;
-    delay=(2e-6)*skipLines;
-    %numRows=4194304/skipLines;%4775129;%4755;
+    delay=(2e-6)*skipLines;%This is based on the digitization rate of our fast digitizer
 	kmax=40000;
     cnum = [8 17 31 41 50 61 70 84 94];
     shotbase=int32(shotn(1,1)/1000)*1000;
@@ -14,10 +13,10 @@ function PowerSpec(shotn)
             shotn(1,n)=shotbase+shotn(1,n);
         endif
         for m=1:length(cnum)
-            %Open the file
+            %Open the file to count the lines, then reopen from the beginning for reading. Skip the first six lines.
             fileName=strcat("PowerSpec",int2str(shotn(1,n)),"-Channel",int2str(cnum(m)),".txt");
             fid=fopen(fileName,'r');
-            numRows=2^floor(log2(fskipl(fid,Inf)))/skipLines;% Put the semi-colon back!
+            numRows=2^floor(log2(fskipl(fid,Inf)))/skipLines;
             maxTime=(numRows-1)*delay/skipLines;
             fclose(fid);
             fid=fopen(fileName,'r');
@@ -25,15 +24,15 @@ function PowerSpec(shotn)
 
             %Read in the data and find sum
             sum=0;
-            sumx=0;%%%%%%%
             array=zeros(numRows,1);
             for i=1:numRows
                 temp=fgetl(fid);
                 temp=strsplit(temp,"\t");
                 array(i,1)=str2double(temp{3});
                 sum+=array(i,1);
-                %fskipl(fid,skipLines-1);%Comment this line out if running skipLines=1
-                %sumx+=i*str2double(temp{3});%%%%%%%%
+		if skipLines!=1
+                	fskipl(fid,skipLines-1);
+		endif
             endfor
             fclose(fid);
 
@@ -60,3 +59,4 @@ function PowerSpec(shotn)
             
             data=horzcat(f,PS,avg1,avg2);
             data=data(1:83889,:);
+	    %plot(f,avg2)
